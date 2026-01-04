8d642a7e58d07dcca188aeec0dc0aa6c11dd5c73
Oldman1958 <boris_i@bk.ru>
2026-01-04 22:15:28 +0500

    Решение заданий

---
+++ tests/8_1_step12/main.py
@@ -0,0 +1,11 @@
+"""
+Это вариант решения задачи для операционной системы Linux Mint. Для форточек решайте сами :).
+"""
+
+import sys, pprint
+
+pprint.pprint(sys.path)
+
+sys.path.append('/home/oldman_mint_ssd/PycharmProjects/Conscious python/tests/8_1_step12/folder1/folder2/mod1')
+
+pprint.pprint(sys.path)
